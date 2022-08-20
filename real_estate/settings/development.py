6e67b26b6a05ccfec5a96c36bbab0075c901ef8f
Shishir Gupta <shishirgupta2011@gmail.com>
2022-08-20 10:45:13 +0530

    chore: split settings.py files

---
+++ real_estate/settings/development.py
@@ -0,0 +1,16 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": env("POSTGRES_ENGINE"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("PG_HOST"),
+        "PORT": env("PG_PORT"),
+
+    }
+}
